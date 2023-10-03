6f5868078166c33b8892fbd01c7b7e3f4b321449
Durlav <durlavk98@gmail.com>
2023-10-03 19:32:53 +0530

    prod commit

--- src/router.tsx
+++ src/router.tsx
@@ -3,14 +3,11 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
-import Register from "./pages/Register";
 import AACA from "./pages/AACA";
 import Events from "./pages/Events";
 import Contact from "./pages/Contact";
 import Post from "./pages/Post";
 import Event from "./pages/Event";
-import Players from "./pages/Players";
-import Player from "./pages/Player";
 
 const Router = () => {
   return (
@@ -20,11 +17,8 @@ const Router = () => {
         <Route path="/" Component={Home}></Route>
         <Route path="/blog" Component={Blog}></Route>
         <Route path="/blog/:id" Component={Post}></Route>
-        <Route path="/register" Component={Register}></Route>
         <Route path="/events" Component={Events}></Route>
         <Route path="/events/:id" Component={Event}></Route>
-        <Route path="/players" Component={Players}></Route>
-        <Route path="/players/:id" Component={Player}></Route>
         <Route path="/aaca" Component={AACA}></Route>
         <Route path="/contact" Component={Contact}></Route>
         {/* <Route component={NotFound} /> */}
